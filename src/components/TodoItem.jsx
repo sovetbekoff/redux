2cb200d09cef3d18f6e920322c14802a1cf24eb0
sovetbekoff <b.sovetbekoff@gmail.com>
2023-09-26 17:17:17 +0600

    first commit

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import styles from "../index.module.css";
 import { useDispatch } from "react-redux";
 import { changeTodoStatus, deleteTodo } from "../store/todoSlice";
 import { useNavigate } from "react-router-dom";
 
-const TodoItem = ({ todo }) => {
+export default function TodoItem() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   return (
-    <li className={styles.list__item}>
-      <input
-        type="checkbox"
-        onChange={() => dispatch(changeTodoStatus(todo.id))}
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
       />
-      <p className={todo.status ? styles.completed : styles.undone}>
-        {todo.body}
-      </p>
-      <button onClick={() => dispatch(deleteTodo(todo.id))}>Delete</button>
-      <button onClick={() => navigate(`/edit/${todo.id}`)}>Edit</button>
-    </li>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <li className={styles.list__item}>
+          <input
+            type="checkbox"
+            onChange={() => dispatch(changeTodoStatus(todo.id))}
+          />
+          <p className={todo.status ? styles.completed : styles.undone}>
+            {todo.body}
+          </p>
+          <button onClick={() => dispatch(deleteTodo(todo.id))}>Delete</button>
+          <button onClick={() => navigate(`/edit/${todo.id}`)}>Edit</button>
+        </li>
+      </CardActions>
+    </Card>
   );
-};
-
-export default TodoItem;
+}
